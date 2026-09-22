f51364976233a2e7e0179ef72cda132e5f71b4a4
Idahk19 <idahkarwitha9@gmail.com>
2026-09-22 13:07:27 +0300

    feat: add AdminDashboard route for admin access

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import AdminDashboard from "./pages/Admindashboard";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+  
       </Routes>
     </BrowserRouter>
   );
