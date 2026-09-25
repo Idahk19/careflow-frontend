ab31b2c8b13cc65070f37de702b5567acb0871a7
Idahk19 <idahkarwitha9@gmail.com>
2026-09-25 09:00:10 +0300

    feat: remove CompletedAppointments route from App component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Profile from "./pages/Profile";
 import DoctorDashboard from "./pages/DoctorDashboard";
 import DoctorQueue from "./pages/DoctorQueue";
 import DoctorAppointments from "./pages/DoctorAppointments";
-import CompletedAppointments from "./pages/CompletedAppointments";
 import DoctorAllAppointments from "./pages/DoctorAllappointments";
 
 function App() {
@@ -33,7 +32,6 @@ function App() {
         <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
         <Route path="/doctor/queue" element={<DoctorQueue />} />
         <Route path="/doctor/appointments" element={<DoctorAppointments />} />
-        <Route path="/doctor/completed" element={<CompletedAppointments />} />
         <Route path="/doctor/allappointments" element={<DoctorAllAppointments />} />
       </Routes>
     </BrowserRouter>
